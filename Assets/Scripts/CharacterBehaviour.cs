41b9ba4819fccb55e2a3d4565f64c9cbaa71361f
Matthew Williamson <chuwilliamson@gmail.com>
2017-05-25 11:37:44 -0500

    add input

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CharacterBehaviour : MonoBehaviour {
+public class CharacterBehaviour : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            Debug.Log("test");
+    }
 }
