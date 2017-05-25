0a6b985865aaafb401d2878da5c69302d3c56d84
Matthew Williamson <chuwilliamson@gmail.com>
2017-05-25 16:30:06 -0500

    gamestate#1

---
+++ Assets/Scripts/ScriptableObjects/GameStates/GameStateSingleton.cs
@@ -0,0 +1,18 @@
+﻿
+
+using UnityEngine;
+[CreateAssetMenu(fileName = "GameState", menuName = "Singletons/GameState")]
+public class GameStateSingleton : ScriptableSingleton<GameStateSingleton>
+{
+    /*
+    intro scene to game scene
+    game scene to intro scene
+    game scene to credit scene
+    */
+    public enum GameState
+    {
+        INTRO = 0,
+        RUNNING = 1,
+        EXIT = 2,
+    }
+}
