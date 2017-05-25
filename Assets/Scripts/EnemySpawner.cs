2a27bf29a9c6d377521e26675362bbfa2468f8d0
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-25 15:42:56 -0500

    EnemyTowerSpawner
    The two scripts created for this ticket are the
    EnemyTowerSpawningBehavior and EnemySpawner.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "Tower/EnemyTower", order = 1)]
+public class EnemySpawner : ScriptableObject
+{
+    public GameObject EnemyPrefab;
+    public float SpawnDelayInSeconds;
+
+    public void SpawnEnemy(Vector3 spawn)
+    {
+        var enemy = Instantiate(EnemyPrefab);
+        enemy.transform.position = spawn;        
+    }
+}
---
+++ Assets/Scripts/EnemyTowerSpawningBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class EnemyTowerSpawningBehaviour : MonoBehaviour
+{
+    public EnemySpawner SpawnerConfig;
+    public Vector3 SpawnOffsetFromRoot;
+    [SerializeField]
+    private float timer;
+
+    void Start()
+    {
+        if(!SpawnerConfig)
+            Debug.LogError("No spawner config set");
+        SpawnerConfig = Instantiate(SpawnerConfig);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= SpawnerConfig.SpawnDelayInSeconds)
+        {
+            timer = 0;
+            SpawnerConfig.SpawnEnemy(this.transform.position + SpawnOffsetFromRoot);   
+        }
+    }
+}
