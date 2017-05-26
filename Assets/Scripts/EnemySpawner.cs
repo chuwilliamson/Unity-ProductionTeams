111459afc30cae4fc74cbac8727de6e83ca800be
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-26 10:16:02 -0500

    EnemytowerSpawningBehaviour (#5)
    * EnemyTowerSpawner
    
    The two scripts created for this ticket are the
    EnemyTowerSpawningBehavior and EnemySpawner.
    
    * EnemySpawner fix
    
    The enemy spawner now has a behavior to spawn a default enemy if the
    value for enemy prefab is not set.
    
    * Removed using
    
    * DefaultEnemyPrefab
    
    * Scriptable Object changes
    
    * Implemented feedback
    
    Added a visual representation for the user to see where the Enemy will
    be spawned by displaying a gizmo in the editor window.
    
    * Asset changes

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "Tower/EnemyTower", order = 1)]
+public class EnemySpawner : ScriptableObject
+{
+    public GameObject EnemyPrefab;
+    public float SpawnDelayInSeconds;
+    private bool IsInitialized;
+
+    public void Initialize()
+    {
+        IsInitialized = true;
+    }
+
+    public void SpawnEnemy(Vector3 spawn)
+    {
+        if (!IsInitialized)
+        {
+            Debug.LogError("Scriptable Object was not Initialized properly. Be sure to invoke the Initialize function");
+            return;
+        }
+        GameObject enemy;
+        if (EnemyPrefab == null)
+        {
+            enemy = Instantiate(Resources.Load("DefaultEnemy")) as GameObject;
+        }
+        else
+            enemy = Instantiate(EnemyPrefab);
+
+        enemy.transform.position = spawn;        
+    }
+}
