4319a6747006aba064122413c30a4e77d2e487ad
Matthew Williamson <chuwilliamson@gmail.com>
2017-06-07 18:20:04 -0500

    tower animation

---
+++ Assets/Scripts/TowerAudioBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class TowerAudioBehaviour : MonoBehaviour
+{
+    public AudioSource ac;
+    public AudioClip shotFired;
+
+    private void Start()
+    {
+        ac = GetComponent<AudioSource>();
+        GetComponent<TowerShootingBehaviour>().OnShotFiredBegin.AddListener(PlayShotFired);
+    }
+    // Use this for initialization
+    public void PlayShotFired(GameObject go)
+    {
+        ac.clip = shotFired;
+        ac.Play();
+    }
+}
