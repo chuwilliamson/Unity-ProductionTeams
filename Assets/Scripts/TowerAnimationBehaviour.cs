c17d9eda3298da3f46e08daeed300aa28ca45764
Matthew Williamson <chuwilliamson@gmail.com>
2017-06-07 18:20:04 -0500

    tower aniation

---
+++ Assets/Scripts/TowerAnimationBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this will be on the playertower
+
+public class TowerAnimationBehaviour : MonoBehaviour
+{
+    Animator anim;
+	// Use this for initialization
+	void Start ()
+	{
+	    anim = GetComponent<Animator>();
+	    GetComponent<TowerShootingBehaviour>().OnShotFiredStart.AddListener(ShotFired);
+	}
+
+    public void ShotFired(GameObject go)
+    {
+        anim.SetTrigger("shotfired");
+    }
+	
+}
