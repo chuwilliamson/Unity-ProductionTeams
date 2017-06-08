cc462386a7eaa2d2e9cd526f7a94e33e91123b64
Matthew Williamson <chuwilliamson@gmail.com>
2017-06-08 12:07:16 -0500

    tower audio prefab change

---
+++ Assets/Editor/DontImportMaterials.cs
@@ -0,0 +1,11 @@
+﻿ 
+using UnityEditor; 
+
+public class DontImportMaterials : AssetPostprocessor
+{
+    public void OnPreprocessModel()
+    {
+        var modelImporter = (ModelImporter)assetImporter;
+        modelImporter.importMaterials = false;
+    }
+}
